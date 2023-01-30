b6edfd23b5e64ae44260e9e895f87449a20e76b6
砸松果 <90502461+za-songguo@users.noreply.github.com>
2023-01-30 18:56:55 +0800

    第一次提交（第9节）

---
+++ server/src/article/delete.rs
@@ -0,0 +1,21 @@
+use std::sync::Arc;
+
+use ntex::web::types::{Path, State};
+
+use crate::{errors::CustomError, models::user::Admin, AppState};
+
+/// 删除文章
+/// 需要管理员权限
+pub async fn delete_article(
+    _: Admin,
+    id: Path<(u32,)>,
+    state: State<Arc<AppState>>,
+) -> Result<String, CustomError> {
+    let db_pool = &state.db_pool;
+
+    sqlx::query!("DELETE FROM articles WHERE id = $1", id.0 as i32)
+        .execute(db_pool)
+        .await?;
+
+    Ok("删除文章成功！".into())
+}
